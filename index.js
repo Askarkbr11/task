e49e9bbd287e937f753e355a4583cf8ec1a4c5d2
Askar <Askarkabeer10@gmail.com>
2022-09-23 11:58:46 +0530

    bug fixed

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ require("dotenv").config()
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const customCss = fs.readFileSync((process.cwd()+"/swagger.css"), 'utf8');
-const PORT = process.env.PORT
+const PORT = process.env.PORT||5000
 const DB_URL = process.env.MONGODB_CLOUD_URL
 const connectToDb = require("./config/db-config")
 const UserRoutes = require("./routes/user")
 const TaskRoutes = require("./routes/task")
+const host ="0.0.0.0";
 
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {customCss}));
@@ -19,7 +20,7 @@ connectToDb(DB_URL)
 app.use("/",UserRoutes)
 app.use("/api/",TaskRoutes)
 
-app.listen(PORT,err=>{
+app.listen(PORT,host,err=>{
     if(err) throw err
     console.log(`server is running on ${PORT}`)
 })
